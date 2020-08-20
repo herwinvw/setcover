29862691ab0cba2f52ab77450ac8b8638b1a5efd
herwin <h.vanwelbergen@alumnus.utwente.nl>
2020-08-20 22:10:04 +0200

    bb solver version 1

---
+++ bb_solver.py
@@ -0,0 +1,89 @@
+import time
+from greedy_solver import best_valued_set, greedy_solver
+
+
+def _is_valid(sets, current_taken, item_count):
+    covered = get_covered(sets, current_taken)
+    for s in sets[len(current_taken):len(sets)]:
+        covered |= s.items
+    return len(covered) == item_count
+
+
+def _back_track(sets, current_taken, item_count):
+    last_branch = None
+    for i, t in enumerate(current_taken):
+        if t == 1:
+            last_branch = i
+
+    if last_branch is None:
+        return None
+
+    new_taken = current_taken[0:last_branch+1]
+    new_taken[last_branch] = 0
+
+    if not _is_valid(sets, new_taken, item_count):
+        new_taken = _back_track(sets, new_taken, item_count)
+    return new_taken
+
+
+def get_covered(sets, current_taken):
+    covered = set()
+    for i, s in enumerate(sets[0:len(current_taken)]):
+        if current_taken[i]:
+            covered |= s.items
+    return covered
+
+
+def get_cost(sets, current_taken):
+    return sum([sets[i].cost*t for i, t in enumerate(current_taken)])
+
+
+def opt_heuristic(remaining_sets, covered, item_count):
+    _, cost_per_item = best_valued_set(remaining_sets, covered)
+    return (item_count-len(covered))*cost_per_item
+
+
+def _bb_solve(sets, item_count, best_taken, best_cost, max_time):
+    covered = set()
+    start = time.time()
+    optimal = 0
+    current_taken = []
+    current_cost = 0
+    while (not max_time) or (time.time()-start) < max_time:
+        backtrack = False
+
+        # covered everything?
+        if len(covered) == item_count:
+            backtrack = True
+            if current_cost < best_cost:
+                best_cost = current_cost
+                best_taken = current_taken
+
+        # every path on the branch leads to > best_value => backtrack
+        min_cost = current_cost + \
+            opt_heuristic(sets[len(current_taken):item_count],
+                          covered, item_count)
+        if min_cost > best_cost:
+            backtrack = True
+
+        if backtrack:
+            current_taken = _back_track(sets, current_taken, item_count)
+            if current_taken is None:
+                optimal = 1
+                break
+            else:
+                covered = get_covered(sets, current_taken)
+                current_cost = get_cost(sets, current_taken)
+        else:
+            s = sets[len(current_taken)]
+            covered |= s.items
+            current_cost += s.cost
+            current_taken.append(1)
+
+    return best_taken, optimal
+
+
+def bb_solver(sets, item_count, max_time=None):
+    best_taken = greedy_solver(sets, item_count)
+    best_cost = get_cost(sets, best_taken)
+    return _bb_solve(sets, item_count, best_taken, best_cost, max_time)
--- greedy_solver.py
+++ greedy_solver.py
@@ -1,20 +1,21 @@
 def best_valued_set(sets, covered):
-    max_value = 0
+    min_value = float('inf')
     sel_s = None
     for s in sets:
         uncovered = set(s.items) - covered
-        value = len(uncovered)/s.cost
-        if value>max_value:
-            max_value = value
-            sel_s = s
-    return sel_s
+        if len(uncovered)>0:
+            cost_per_uncovered = s.cost/len(uncovered)
+            if cost_per_uncovered<min_value:
+                min_value = cost_per_uncovered
+                sel_s = s
+    return sel_s, min_value
 
 def greedy_solver(sets, item_count):
     covered = set()
     
     solution = [0]*len(sets)
     while(len(covered) < item_count):
-        s = best_valued_set(sets, covered)
+        s, _ = best_valued_set(sets, covered)
         solution[s.index] = 1
         covered |= set(s.items)
         
